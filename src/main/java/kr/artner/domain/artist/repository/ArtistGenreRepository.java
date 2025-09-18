def6db84b8ee39b5732aa8c7b6c634ca32f58700
김구 <skk3010g@gmail.com>
2025-09-19 02:40:46 +0900

    fix: loginmember 인증 이슈 해결 및 아티스트 생성 응답 수정

--- src/main/java/kr/artner/domain/artist/repository/ArtistGenreRepository.java
+++ src/main/java/kr/artner/domain/artist/repository/ArtistGenreRepository.java
@@ -2,7 +2,11 @@ package kr.artner.domain.artist.repository;
 
 import kr.artner.domain.artist.entity.ArtistGenre;
 import kr.artner.domain.artist.entity.ArtistGenreId;
+import kr.artner.domain.artist.entity.ArtistProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArtistGenreRepository extends JpaRepository<ArtistGenre, ArtistGenreId> {
+    List<ArtistGenre> findByArtistProfile(ArtistProfile artistProfile);
 }
